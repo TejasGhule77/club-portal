f4682821659185ca618f919767ab3b45cd37a2f6
Tejas Ghule <tejasghule2395@gmail.com>
2026-07-27 18:42:26 +0530

    Update vite urlt

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://club-portal-1-ms4q.onrender.com',
         changeOrigin: true,
       },
     },
